ab4a769779f7db005ea643bfc2f457634cfacd53
Bonnie Acuna <bonnieacuna@Bonnies-MacBook-Pro.local>
2018-11-23 18:02:38 -0700

    homework6-3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,15 +16,16 @@ $(document).ready(function () {
             .then(function (response) {
                 console.log(response);
                 var results = response.data;
-                
-                // var still = results[i].images.fixed_height_still.url;
 
                 for (var i = 0; i < results.length; i++) {
-                    
-
+                    var gif = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
                     var moviesDiv = $("<div>");
                     var p = $("<p>").text("Rating: " + results[i].rating);
                     var moviesImage = $("<img>");
+                    moviesImage.attr("data-gif", gif);
+                    moviesImage.attr("data-still", still);
+                    moviesImage.attr("data-state", "gif");
                     moviesImage.addClass("movies-image")
                     moviesImage.attr("src", results[i].images.fixed_height.url);
                     moviesDiv.append(p);
@@ -56,17 +57,17 @@ $(document).ready(function () {
         showGifs(movies);
     });
 
-    // $(document).on("click", ".movies-image", function() {
-    //     var image= $(this).attr("data-state");
-    //     if (image === still) {
-    //         $(this).attr("src", $(this).attr("data-animate"));
-    //         $(this).attr("data-state", "animate");
-    //       } else {
-    //         $(this).attr("src", $(this).attr("data-still"));
-    //         $(this).attr("data-state", "still");
-    //       }
-    // });
-
+    $(document).on("click", ".movies-image", function() {
+        var state = $(this).attr("data-state");
+        if (state === still) {
+            $(this).attr("src", $(this).attr("data-gif"));
+            $(this).attr("data-state", "gif");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+    });
+    displayButtons();
     
 
     $("#add-movie").on("click", function(event) {
