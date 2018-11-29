afad321196608d5ef9e21598968b434fda13fb76
Bonnie Acuna <bonnieacuna@Bonnies-MacBook-Pro.local>
2018-11-28 17:39:35 -0700

    homework6-4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            movies + "&api_key=WHPbjD6d9ErGX1a7afOw7khTs4tdlBnW&limit=10";
+            movies + "&api_key=WHPbjD6d9ErGX1a7afOw7khTs4tdlBnW&limit=9";
 
         $.ajax({
             url: queryURL,
@@ -16,20 +16,21 @@ $(document).ready(function () {
             .then(function (response) {
                 console.log(response);
                 var results = response.data;
-
+                $("#gifs").empty();
                 for (var i = 0; i < results.length; i++) {
                     var gif = results[i].images.fixed_height.url;
                     var still = results[i].images.fixed_height_still.url;
                     var moviesDiv = $("<div>");
+                    moviesDiv.addClass("movies-image-container")
                     var p = $("<p>").text("Rating: " + results[i].rating);
                     var moviesImage = $("<img>");
                     moviesImage.attr("data-gif", gif);
                     moviesImage.attr("data-still", still);
-                    moviesImage.attr("data-state", "gif");
-                    moviesImage.addClass("movies-image")
-                    moviesImage.attr("src", results[i].images.fixed_height.url);
-                    moviesDiv.append(p);
+                    moviesImage.attr("data-state", "still");
+                    moviesImage.addClass("movies-image");
+                    moviesImage.attr("src", results[i].images.fixed_height_still.url);
                     moviesDiv.append(moviesImage);
+                    moviesDiv.append(p);
                     $("#gifs").prepend(moviesDiv);
                 }
             });
@@ -41,8 +42,10 @@ $(document).ready(function () {
 
         for (var i = 0; i < topics.length; i++) {
 
-            var moviesButton = $("<button>");
+            var moviesButton = $("<div>");
             moviesButton.addClass("movies-btn");
+            moviesButton.addClass("btn");
+            moviesButton.addClass("btn-danger");
             moviesButton.attr("data-name", topics[i]);
             moviesButton.text(topics[i]);
             $("#movies").append(moviesButton);
@@ -55,19 +58,22 @@ $(document).ready(function () {
         var movies = $(this).attr("data-name");
         console.log(movies)
         showGifs(movies);
+        $("#movie-input").val("");
     });
 
     $(document).on("click", ".movies-image", function() {
         var state = $(this).attr("data-state");
-        if (state === still) {
+        if (state === "still") {
             $(this).attr("src", $(this).attr("data-gif"));
             $(this).attr("data-state", "gif");
+            console.log("still clicked");
           } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
+            console.log("animate clicked");
           }
     });
-    displayButtons();
+    
     
 
     $("#add-movie").on("click", function(event) {
@@ -77,6 +83,8 @@ $(document).ready(function () {
         displayButtons();
     });
     displayButtons();
+
+
 
 
 
